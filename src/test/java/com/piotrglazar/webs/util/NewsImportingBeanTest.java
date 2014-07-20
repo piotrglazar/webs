bf491fca2ee8baf2cf1aee426b53e9ac1e50b0be
Piotr Glazar <piotr.glazar@allegro.pl>
2014-07-20 16:51:20 +0200

    JMX Beans

---
+++ src/test/java/com/piotrglazar/webs/util/NewsImportingBeanTest.java
@@ -0,0 +1,41 @@
+package com.piotrglazar.webs.util;
+
+import com.piotrglazar.webs.business.NewsImporters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NewsImportingBeanTest {
+
+    @Mock
+    private NewsImporters newsImporters;
+
+    @InjectMocks
+    private NewsImportingBean newsImportingBean;
+
+    @Test
+    public void shouldFetchAllNews() {
+        // when
+        newsImportingBean.importNews();
+
+        // then
+        verify(newsImporters).fetchAllNews();
+    }
+
+    @Test
+    public void shouldParticularFetchNews() {
+        // given
+        final int newsImporterIndex = 1;
+
+        // when
+        newsImportingBean.importNews(newsImporterIndex);
+
+        // then
+        verify(newsImporters).fetchNews(newsImporterIndex);
+    }
+}
